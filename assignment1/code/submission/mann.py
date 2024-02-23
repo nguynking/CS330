[Assignment 1] Fix wrong dim Problem 2

assignment1/code/submission/mann.py
@@ -37,6 +37,8 @@ class MANN(nn.Module):
         """
         #############################
         ### START CODE HERE ###
+        B = input_labels.shape[0]
+
         # Step 1: Concatenate the full (support & query) set of labels and images
         x = torch.cat([input_images, input_labels], dim=-1) # (B, K+1, N, 784 + N)
 
@@ -44,7 +46,7 @@ class MANN(nn.Module):
         x[:, -1, :, 784:] = torch.zeros_like(input_labels)[:, -1]
 
         # Step 3: Pass the concatenated set sequentially to the memory-augmented network
-        support = x.reshape(-1, self.num_classes, self.num_classes + 784).float()
+        support = x.reshape(B, self.samples_per_class * self.num_classes, self.num_classes + 784).float()
         out, _ = self.layer1(support)
         out, _ = self.layer2(out)
 
@@ -77,7 +79,7 @@ class MANN(nn.Module):
         # Step 1: extract the predictions for the query set
         query_preds = preds[:, -1].reshape(-1, self.num_classes)
         # Step 2: extract the true labels for the query set and reverse the one hot-encoding  
-        query_labels = torch.argmax(labels[:, -1], dim=2).reshape(-1)
+        query_labels = torch.argmax(labels[:, -1], dim=-1).reshape(-1)
         # Step 3: compute the Cross Entropy Loss for the query set only!
         loss = F.cross_entropy(query_preds, query_labels)
         ### END CODE HERE ###